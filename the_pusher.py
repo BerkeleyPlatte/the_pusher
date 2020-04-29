49b51ade001006f495be191bf2cd53acc6fae837
unknown <berkeleyplatte989@gmail.com>
2020-04-28 20:00:14 -0500

    working

--- the_pusher.py
+++ the_pusher.py
@@ -1,9 +1,21 @@
 from datetime import datetime
 import subprocess
+from imports import insert
 
 today = datetime.today().strftime('%Y-%m-%d')
 
-with open("the_pusher.txt", "w") as update:
+with open('the_pusher.txt', 'w') as update:
     update.write(f'{today}')
 
-subprocess.run("echo 'hello world'", check=True, shell=True)
+subprocess.run(
+    [
+        'cd workspace',
+        'cd the_pusher',
+        'git add .',
+        'git commit -m "minor changes"',
+        'git push origin master',
+        f'{insert}'
+    ], stdout=subprocess.PIPE,
+       stderr=subprocess.STDOUT,
+       shell=True
+)
